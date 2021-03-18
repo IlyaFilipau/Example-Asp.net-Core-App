25281b07c6df1c7eeb456f32916b82d7dba93c32
Ilya <ilya.375447567562@gmail.com>
2021-03-18 22:19:27 +0300

    leason 2

--- Asp.net_Core_WebApplication/Startup.cs
+++ Asp.net_Core_WebApplication/Startup.cs
@@ -9,21 +9,28 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Asp.net_Core_WebApplication
 {
-    public class Startup
+    public class  Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container. 123
+        // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
